de78b7033d249f90031c884c193366ed9d337f30
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2020-11-10 16:23:58 -0500

    new update 1

--- src/com/company/Hello2.java
+++ src/com/company/Hello2.java
@@ -3,5 +3,8 @@ package com.company;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Heloo 321");
+        //new lane 1
+        //new lane 2
+        //new lane 3
     }
 }
